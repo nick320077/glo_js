4429f5e73621cb2fd3b78abb9d4ab42f4f1fa181
nick32007 <kobets32@gmail.com>
2022-01-30 20:35:08 +0200

    did my homework hopefully correct

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+
+
+const title = 1;
+const screens = 5;
+const screenPrice = 1;
+const rollback = 'checkout';
+const fullPrice = 1000;
+const adaptive = 'responsive';
+
+alert('hello alert!!!');
 console.log('Hello, console!!!');
