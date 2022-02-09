7e070da1877984efe239c6139bec71d008e3afcc
nick32007 <kobets32@gmail.com>
2022-02-10 01:09:42 +0200

    дз номер 8

--- script.js
+++ script.js
@@ -18,68 +18,80 @@
 
 const appData = {
    title: '',
-   screens: '',
+   screens: [],
    screenPrice: 0,
    adaptive: true,
    rollback: 10,
    allServicePrices: 0,
    fullPrice: 0,
    servicePercentPrice: 0,
-   service1: '',
-   service2: '',
+   services: {},
+   
+   start: function() {
+    appData.asking();
+    appData.addPrices();
+    appData.getFullPrice();
+    appData.getServicePercentPrices();
+    appData.getTitle();
+    appData.logger();
+   },
+
+    isNumber: function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+   },
+
    asking: function() {
     appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-    appData.screens = prompt(' Какие типы екранов нужно разработать?', 'Простые, Сложные');
-    
+   
+   for(let i = 0; i < 2; i++) {
+        let name = prompt(' Какие типы екранов нужно разработать?');
+        let price = 0;
+
+        do {
+            price = prompt('Сколько будет стоить данная работа?');
+        } while (!appData.isNumber(price));
+        appData.screens.push({ id: i, name: name, price: price })
+
+    }
+
+    for(let i = 0; i < 2; i++) {
+        let name = prompt('Какой дополнительный тип услуги нужен?'); 
+        let price = 0;
+
     do {
-        appData.screenPrice = prompt('Сколько будет стоить данная работа?');
-    } while (!isNumber(appData.screenPrice)) 
+       price = prompt('Сколько это будет стоить?');
+    } while (!appData.isNumber(price));
+
+    appData.services[name] = +price;
+      
+  }
     
     appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-}
-}
+   },
 
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-
-const getAllServicePrices = function() {
-     let sum = 0;
-
-     for(let i = 0; i < 2; i++) {
-         let price = 0;
-       if(i === 0) { 
-         appData.service1 = prompt('Какой дополнительный тип услуги нужен?');  
-     } else if(i === 1) {
-         appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
+   addPrices: function() {
+    for(let screen of appData.screens) {
+        appData.screenPrice += +screen.price;
      } 
 
-     do {
-        price = prompt('Сколько это будет стоить?');
-     } while (!isNumber(price))
-       sum += +price;
-   }
+     for(let key in appData.services) {
+        appData.allServicePrices += appData.services[key];
 
-  return sum;
-     
-}
+    }
+   },
 
-function getFullPrice() {
-    return +appData.screenPrice + appData.allServicePrices;
-}
+ getFullPrice: function() {
+    appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
+},
 
-const getServicePercentPrices = function() {
-    return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-}
+ getServicePercentPrices: function() {
+    appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+},
 
-const getTitle  = function() {
-    return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLocaleLowerCase();
-}
-
-
-
-const getRollbackMessage = function(price) {
+ getTitle: function() {
+    appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLocaleLowerCase();
+},
+getRollbackMessage: function(price) {
       if(price >= 30000) {
         return 'Даем скидку в 10%';
     } else if(price >= 15000 && price < 30000) {
@@ -89,15 +101,17 @@ const getRollbackMessage = function(price) {
     } else {
         return 'Что-то пошло не так';
     }
-} 
+}, 
+ logger() {
+    console.log(appData.fullPrice);
+    console.log(appData.servicePercentPrice);
+    console.log(appData.screens);
+  }
 
-appData.asking();
-appData.allServicePrices = getAllServicePrices();
-appData.fullPrice = getFullPrice();
-appData.servicePercentPrice = getServicePercentPrices();
-appData.title = getTitle();
-console.log(appData.fullPrice);
-console.log(appData.servicePercentPrice);
+}
+
+appData.start();
+
 
 
 
