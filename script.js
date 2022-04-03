67860afe16200d250f0312d47b42ae9d467d3146
nick32007 <kobets32@gmail.com>
2022-04-03 15:10:48 +0300

    dz02

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollbac;
-let fullPrice;
-let adaptive;
+let title = 'Калькулятор Верстки';
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+console.log(screens.toLocaleLowerCase().split());
+let screenPrice = 1000;
+console.log( "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+let rollbac = 10;
+let fullPrice = 10000;
+console.log( "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+console.log(typeof fullPrice);
+let adaptive = true;
+console.log(typeof adaptive);
+let rollbackPrice = (fullPrice * (rollback / 100));
 
-console.log("hello!");
-alert('Yo!');
