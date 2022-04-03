569aa6baf868846708a43a3cd3ec2e22ca9a6f97
nick32007 <kobets32@gmail.com>
2022-04-03 17:30:43 +0300

    dz03

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-let title = 'Калькулятор Верстки';
-console.log(typeof title);
-let screens = "Простые, Сложные, Интерактивные";
-console.log(screens.length);
-console.log(screens.toLocaleLowerCase().split());
-let screenPrice = 1000;
-console.log( "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
-let rollbac = 10;
-let fullPrice = 10000;
-console.log( "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
-console.log(typeof fullPrice);
-let adaptive = true;
-console.log(typeof adaptive);
-let rollbackPrice = (fullPrice * (rollback / 100));
 
+
+
+
+
+
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let rollback = 10;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+
+
+if(fullPrice >= 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 5%");
+} else if (fullPrice >= 0 && fullPrice < 15000) {
+    console.log("Скидка не предусмотрена");
+} else {
+    console.log("Что то пошло не так");
+}
+
+
+console.log(servicePercentPrice);
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(adaptive);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
